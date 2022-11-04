23d7e59f4f90206f55f4c6bbc0fd10a8be392fb8
Msmithma <msmithma@iu.edu>
2022-11-03 22:09:53 -0400

    week 9

---
+++ Week 9/Show info/app.js
@@ -0,0 +1,22 @@
+//Madison Smith-Majors 11/3
+
+//Show info
+
+//Create an application with an array of strings, at least three. 
+//Create three buttons that are associated with those strings. 
+//(For instance, you might have 'home', 'about', and 'art'), and a div to display those strings. 
+//Using a single function, make it so that when a button is clicked, the div displays the string associated with it in that array. 
+//("home" button shows index 0, "about" shows index 1, so on..).
+
+//Hint: 
+//You can include an argument in your onclick function invocation: 
+//<button onclick="doThing(12)">This rules</button>
+
+
+
+ //create array of strings 
+ const store = ["home", "about", "art"]; 
+ let index = store.indexOf("art", 2); 
+
+ object.onclick = function(){Showsomething};
+ 
